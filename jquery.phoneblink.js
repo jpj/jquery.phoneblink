726bf1b1eea0d55300dea73d79770ad022335446
Joshua Johnson <joshpauljohnson@gmail.com>
2012-05-16 11:45:49 -0400

    Added support for browsers that do not support the blink tag.

--- jquery.phoneblink.js
+++ jquery.phoneblink.js
@@ -1,10 +1,31 @@
 (function($) {
 
+	// Non-blink browser support
+	var testBlinkEl = document.createElement('blink');
+	var hasBlinkSupport = testBlinkEl.toString() != '[object HTMLUnknownElement]';
+	function blink(selector){
+		$(selector).animate({
+			opacity:0
+		}, 50, "linear", function() {
+			$(this).delay(400);
+			$(this).animate({
+				opacity:1
+			}, 50, function() {
+				blink(this);
+			});
+			$(this).delay(400);
+		});
+	}
+
 	$.fn.phoneblink = function() {
 		return this.each(function() {
 			$(this).html($(this).html().replace(/[1]{0,1}[-]{0,1}[\(]{0,1}[0-9]{3}[\)]{0,1}[ ]{0,1}[-]{0,1}[0-9]{3}-[0-9]{4}/g, function(phone) {
 				return '<blink>'+phone+'</blink>';
 			}));
+
+			if (!hasBlinkSupport) {
+				blink($(this));
+			}
 		});
 	};
 
